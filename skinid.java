73692854581ecc36df7904948a43689504d5c9cb
hexaov91 <60669781+hexaov91@users.noreply.github.com>
2021-04-19 05:24:19 +0800

    Add files via upload

---
+++ skinid.java
@@ -0,0 +1,35 @@
+a111.clear();
+FileUtil.listDir("/storage/emulated/0/Download/130_GongBenWuZang/skill/", a111);
+//skill資料夾的路徑
+a111N = a111.size() - 1;
+oo = new TimerTask() {
+	@Override
+	public void run() {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				s = FileUtil.readFile(a111.get((int)(a111N)));
+				int c =s.indexOf(
+				"\"skinId\" value=\"");
+				
+				if (!(c == -1)) {
+					ss=s.substring(c); 
+					
+					int cc =ss.indexOf(" refParamName" );
+					
+					s2=s.substring(c+16,cc+c-1); 
+					FileUtil.writeFile(a111.get((int)(a111N)), FileUtil.readFile(a111.get((int)(a111N))).replace("\"skinId\" value=\"".concat(s2.substring((int)(0), (int)(3)).concat("00")), "\"skinId\" value=\"".concat(s2.substring((int)(0), (int)(3)).concat("99"))));
+					FileUtil.writeFile(a111.get((int)(a111N)), FileUtil.readFile(a111.get((int)(a111N))).replace("\"skinId\" value=\"".concat(s2), "\"skinId\" value=\"".concat(s2.substring((int)(0), (int)(3)).concat("00"))));
+				}
+				else {
+					s = "";
+				}
+				a111N = a111N - 1;
+				if (a111N == 0) {
+										oo.cancel();
+				}
+			}
+		});
+	}
+};
+_timer.scheduleAtFixedRate(oo, (int)(100), (int)(10));
